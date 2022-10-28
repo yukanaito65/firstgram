254ea8776b20bb47b07cda8a47af9faa2e10d6ec
Ayaka Mizuno <ayaka.mizuno@rakus-partners.co.jp>
2022-10-28 16:33:56 +0900

    型定義、コンポーネントファイル作成

---
+++ react-sample/src/types/types.ts
@@ -0,0 +1,27 @@
+export type User = {
+  userId: number;
+  userName: string;
+  name: string;
+  icon: string;
+  email: string;
+  password: string;
+  follow: []; //userIdを格納
+  follower: []; //userIdを格納
+  favoritePost: []; //postIdを格納
+};
+
+export type Post = {
+  postId: number;
+  userId: number; //ユーザーのuserId
+  imageUrl: [];
+  caption: string;
+  postDate: Date;
+  favorites: []; //userNameを格納
+};
+
+export type Comment = {
+  commentId: number;
+  comment: string;
+  commentTimestamp: Date;
+  hashtag: string;
+};
