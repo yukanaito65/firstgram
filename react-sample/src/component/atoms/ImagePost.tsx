c76dcf5f2d7ff550efd7a518a534bcb728da9002
emiri yasuo <emiri.yasuo@rakus-partners.co.jp>
2022-11-04 15:02:11 +0900

    imageコンポーネント完成（CSS未完）

--- react-sample/src/component/atoms/ImagePost.tsx
+++ react-sample/src/component/atoms/ImagePost.tsx
@@ -3,12 +3,11 @@ import React, { useState } from "react";
 import storage from "../../firebase-sec";
 import { getDownloadURL, ref, uploadBytes, uploadBytesResumable } from "firebase/storage";
 import { FirebaseError } from "firebase/app";
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, addDoc, setDoc } from "firebase/firestore"; 
 import { db } from '../../firebase';
-
+import { doc, updateDoc } from "firebase/firestore";
 
 const ImageUploader = () => {
-
     //loadingしているかしてないか監視する
     const [loading, setloading] = useState(false);
     // アップロードが完了したか確認する
@@ -16,29 +15,23 @@ const ImageUploader = () => {
     // 画像のsrc
     const [imgSrc, setImgSrc] = useState("");
 
-  const OnFileUploadToFirebase = (e:any) => {
-    // console.log(e.target.files[0].name);
+const OnFileUploadToFirebase = (e:any) => {
     const file = e.target.files[0];
+    
     // パスと名前で参照を作成
     const storageRef = ref(storage,"image/"  + file.name);
-    // uploadBytes(storageRef, file).then((snapshot) => {
-    //   console.log("Uploaded a blob or file!");
-    // });
-
     // 画像のアップロード
     const uploadImage = uploadBytesResumable(storageRef, file);
-    
     // 画像のダウンロード
-        getDownloadURL(storageRef)
-        .then(url => {
-        setImgSrc(url)
-        console.log("画像のurlは" + url)
-        })
-        .catch(err => console.log(err))
+    getDownloadURL(storageRef)
+    .then(url => {
+    setImgSrc(url)
+    console.log("画像のurlは" + url)
+    })
+    .catch(err => console.log(err))
 
     // 画像のモニタリング
-    uploadImage.on(
-        "state_changed",
+    uploadImage.on("state_changed",
         // upload開始したらloading中になる(loadingがtureになる)
         (snapshot) => {
             setloading(true);
@@ -51,47 +44,41 @@ const ImageUploader = () => {
             setloading(false);
             setIsUploaded(true);
         }
-        )
+    )
     
-// firebase
-// Add a new document with a generated id.
-const docRef = addDoc(collection(db, "post"), {
-  caption: "test",
-//   imagePath: `${ref(storage,"image/"  + file.name)}`
-  imgUrl: `${ref(storage,"image/"  + file.name)}`
-});
-    
-
+// firestoreに画像を追加
+// ドキュメント追加
+// const docRef = addDoc(collection(db, "post"), 
+// {caption: "test",
+//   imgUrl: `${ref(storage,"image/"  + file.name)}`
+// });
+// ドキュメント更新
+// const docImagePost = doc(db, "post", "zbzbfOdWidTHmKOjeyvr");
+// updateDoc(docImagePost, {
+// imgUrl: `${ref(storage,"image/"  + file.name)}`
+// });
 };
-  return (
-    <>
+return (
+<>
     {loading ? (
-    <h2>アップロード中</h2>
-     ) : (
+        <h2>アップロード中</h2>
+    ) : (
         <>
-        {isUploaded ? (
+    {isUploaded ? (
+        // 画面遷移（トップページとか？）
         <h2>アップロード完了</h2>
-        ):(
-      <div>
+    ):(
+        <div>
         <p>JpegかPngの画像ファイル</p>
-          {/* <img src={ImageLogo} alt="imagelogo" /> */}
-        <input 
-        // multiple
-        name="imageURL"
-        type="file"
-        accept=".png, .jpeg, .jpg"
-        onChange={ OnFileUploadToFirebase }
-        />
-    </div>
-        )}
-    </>
+        <input name="imageURL" type="file" accept=".png, .jpeg, .jpg"
+        onChange={ OnFileUploadToFirebase }/>
+        </div>
     )}
-    <img alt="" src={imgSrc} 
-    // onChange={Download}
-    />
-    </>
-    
-  );
+        </>
+    )}
+    <img alt="" src={imgSrc} />
+</>
+);
 };
 
 export default ImageUploader;
