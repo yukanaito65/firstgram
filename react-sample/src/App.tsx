defd433df358db6ec8cdc3b969050d225e8b83b8
Ayaka Mizuno <ayaka.mizuno@rakus-partners.co.jp>
2022-11-07 11:27:18 +0900

    icno表示コンポーネント一旦完成

--- react-sample/src/App.tsx
+++ react-sample/src/App.tsx
@@ -1,8 +1,3 @@
-import Input from './component/atoms/Input';
-import { db } from './firebase';
-// import Icon from './component/atoms/Icon';
-import { useEffect, useState } from 'react';
-import {collection, getDocs} from 'firebase/firestore';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
@@ -11,33 +6,9 @@ import './index.css';
 
 
 function App() {
-    const [users,setUsers] = useState<any[]>([]);
-
-    useEffect(()=>{
-      const userCollectionRef = collection(db, 'user');
-      // console.log(userCollectionRef);
-      getDocs(userCollectionRef).then((querySnapshot)=>{
-        // console.log(querySnapshot);
-        // querySnapshot.docs.forEach((doc)=>console.log(doc));
-        //保存しているフィールドのデータのみ取得する↓
-        // querySnapshot.docs.forEach((doc)=>console.log(doc.data()));
-        setUsers(querySnapshot.docs.map((doc)=>({...doc.data(), id:doc.id})));
-      });
-    },[]);
 
   return (
     <div className="App">
-      {/* <Icon /> */}
-      <div>
-        {users.map((user:any)=>(
-          <div key={user.id}>
-          {/* <img src={user.icon} alt="icon" /> */}
-          {/* <p>{user.name}</p> */}
-          </div>
-        ))}
-      </div>
-
-
      <div className="container">
       <BrowserRouter>
           <Routes>
@@ -47,7 +18,6 @@ function App() {
           </Routes>
       </BrowserRouter>
      </div>
-
     </div>
   );
 }
