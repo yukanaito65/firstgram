dc624cc43f30d248dcb12e968fb6beab76d4f2f7
Yuka Naito <yuka.naito@rakus-partners.co.jp>
2022-11-24 16:47:32 +0900

    settingPageのcss完了

--- react-sample/src/component/atoms/pictures/Icon.tsx
+++ react-sample/src/component/atoms/pictures/Icon.tsx
@@ -57,28 +57,28 @@ function Icon() {
       {!loading && (
         <div
           className="icon-image"
-          style={{
-            borderRadius: "50%",
-            width: "100px",
-            height: "100px",
-            border: "2px, lightgray",
-          }}
+          // style={{
+          //   borderRadius: "50%",
+          //   width: "100px",
+          //   height: "100px",
+          //   border: "2px, lightgray",
+          // }}
         >
           {user && users.icon !== "" ? (
             <img
               src={users.icon}
               alt="icon"
-              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              // style={{ width: "100%", height: "100%", objectFit: "cover" }}
             />
           ) : (
             <img
               src={`${process.env.PUBLIC_URL}/noIcon.png`}
               alt="NoImage"
-              style={{
-                width: "100%",
-                height: "100%",
-                backgroundColor: "#d3d3d3",
-              }}
+              // style={{
+              //   width: "100%",
+              //   height: "100%",
+              //   backgroundColor: "#d3d3d3",
+              // }}
             />
           )}
         </div>
