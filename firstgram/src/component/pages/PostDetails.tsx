146024e4ccc6c5332976e5040e4680b6a9aae55b
emiri yasuo <emiri.yasuo@rakus-partners.co.jp>
2022-11-29 17:54:48 +0900

    css

--- firstgram/src/component/pages/PostDetails.tsx
+++ firstgram/src/component/pages/PostDetails.tsx
@@ -1,6 +1,6 @@
  import { useEffect, useState } from "react";
 import { auth, db } from "../../firebase";
-import {collection,getDoc,doc, deleteDoc, updateDoc, arrayUnion, arrayRemove,} from "firebase/firestore";
+import {collection,getDoc,doc, deleteDoc, updateDoc, arrayUnion, arrayRemove, query, where, getDocs,} from "firebase/firestore";
 import { Link, useLocation } from "react-router-dom";
 import firebasePostDetails from "../utils/firebasePostDetails";
 import { onAuthStateChanged } from "firebase/auth";
@@ -8,7 +8,7 @@ import Footer from "../molecules/Footer";
 import Header from "../molecules/Header";
 import GetLoginUserData from "../utils/GetLoginUserData";
 import FavoriteUpdata from "../utils/FavoriteUpdata";
-import { AiFillHeart,AiOutlineEllipsis,AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
+import { AiFillHeart,AiOutlineClose,AiOutlineEllipsis,AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
 import CommonIcon from "../atoms/pictures/CommonIcon";
 import RemoveKeepButton from "../atoms/button/RemoveKeepButton";
 import AddKeepButton from "../atoms/button/AddKeepButton";
@@ -45,6 +45,11 @@ const [postUserName, setPostUserName] = useState<any>("");
 // iconを格納
 const [icon, setIcon] = useState<any>("");
 
+// selectbutton表示非表示
+const[select,setSelect]=useState<boolean>(false)
+
+// コメントの表示非表示
+const[commentDisplay, setCommentDisplay] = useState<boolean>(false)
 
 // // timeを格納
 // const [time, settime] = useState<any>("");
@@ -62,8 +67,6 @@ const [icon, setIcon] = useState<any>("");
 // const [seco, setSeco] = useState<any>("");
 
 // const currentUserId = auth.currentUser?.uid
-
-
 
 // postlookからデータを持ってくる
 const location = useLocation();
@@ -87,13 +90,13 @@ setLoginUserPost(false)
 })
 
 
+
+
 // 画面遷移したら、firestoreから画像、caption,falolites,commmentを取得、保持
 firebasePostDetails(postid,userid).then((postData)=>{
 
 // setKeepList(loginUserKeep);
 // setDisplayPostId(postId)
-
-
 
 setImgUrl(postData.Imgurl)
 setCaption(postData.Caption)
@@ -137,6 +140,19 @@ const AddComment =async(e:any)=>{
       setDisplayComment(postData.Comments)
       })
       setInputComment("")
+
+      // keeppostとってこれてるか確認
+const DeleteKeepPosts = query(collection(db,"user"),
+where("keepPosts", "array-contains", postid));
+const DeleteKeepPostsUsers:any[]=[];
+const DeleteKeepPostsUser = await getDocs(DeleteKeepPosts);
+DeleteKeepPostsUser.forEach((doc) => {
+    const users =(doc.id, " => ", doc.data()
+//     .keepPosts
+    );
+    DeleteKeepPostsUsers.push(users)
+});
+
 }
 
 const ClickDelition = async(e:any) =>{
@@ -149,17 +165,178 @@ const postUserData = postUserDoc.data();
 // 投稿者のpostを取り出す
 const postUserPost = postUserData?.posts
 
+
 const index = postUserPost.indexOf(postid);
 postUserPost.splice(index, 1)
 console.log(postUserPost)
 await updateDoc(postUserDocRef,{
       posts: postUserPost
 });
+
+
+const DeleteKeepPosts = query(collection(db,"user"),
+where("keepPosts", "array-contains", postid));
+
+const DeleteKeepPostsUsers:any[]=[];
+const DeleteKeepPostsUser = await getDocs(DeleteKeepPosts);
+DeleteKeepPostsUser.forEach((doc) => {
+    const users =(doc.id, " => ", doc.data());
+    DeleteKeepPostsUsers.push(users)
+});
+
+console.log(DeleteKeepPostsUsers)
+
 await deleteDoc(doc(db, "post", postid));
 }
+
+const Select = (e:any) =>{
+      setSelect(true)
+}
+
+const Back =(e:any) =>{
+      setSelect(false)
+}
+
+const CommentDisplay =(e:any) =>{
+      setCommentDisplay(true)
+}
+
+const CommentBack =(e:any) =>{
+      setCommentDisplay(false)
+}
+
 return (
 <>
 <Header show={true} />
+<div>
+{loginUserPost ?(
+<>
+<div style={{display:"flex",alignItems:"center",width:"100%"}}>
+
+{/* <Link to="/profile" state={{ userId: userid}}><img src={icon} alt="icon" style ={{width:"100px",height:"100px"}}/></Link> */}
+<Link to="/profile" state={{ userId: userid}}><CommonIcon icon={icon} /></Link>
+<p style ={{fontSize:"20px",marginLeft:"5px"}}>{postUserName }</p>
+
+{/* <div style={{marginLeft:"auto"}}>
+<AiOutlineEllipsis style={{display:"block"}} size={40} color={"rgb(38, 38, 38)"} onClick={Select}/>
+</div> */}
+
+{/* <div style={{marginLeft:"auto"}}> */}
+{select ? (
+      <>
+      <div style={{marginLeft:"auto"}}>
+         <nav style={{width: "100%",margin:"0 auto "}}>
+          <ul style={{padding:"5px 0",position:"fixed",top: "100px",right: "25%",listStyle: "none",width: "20%",backgroundColor:" #f7f7f7",boxShadow: "0 0px 10px 7px rgb(0 0 0 / 10%)",fontSize: "19px",zIndex:" 800",marginLeft: "auto"}}>
+            <li style={{width: "100%",borderBottom:"2px solid #e7e7e7",padding: "5px 0"}}>
+            <Link to="/PostEditing" state={{postid:postid,userid:userid}}>
+            <button className="nav_menu_btn">編集</button></Link>
+            </li>
+            <li style={{width: "100%",borderBottom:"2px solid #e7e7e7",padding: "5px 0"}}>
+            <Link to="/">
+            <button onClick={ClickDelition} className="nav_menu_btn" >削除</button></Link>
+            </li>
+          </ul>
+        </nav>
+        </div>
+
+      <div style={{marginLeft:"auto"}}>
+      <AiOutlineClose  style={{display:"block"}} size={27} color={"rgb(38, 38, 38)"} onClick={Back} />
+      </div>
+    
+      </>
+):(
+      <>
+<div style={{marginLeft:"auto"}}>
+<AiOutlineEllipsis style={{display:"block"}} size={40} color={"rgb(38, 38, 38)"} onClick={Select}/>
+</div>
+      </>
+)}
+{/* </div> */}
+
+</div>
+
+
+
+<div style={{width:"100%",marginTop:"10px"}} >
+<img src={imgUrl} style={{margin:"auto",display:"block"}} />
+</div>
+
+<div style={{display:"flex"}}>
+
+<div style={{margin:"10px 5px 0px 5px"}}>
+{favorites.includes(loginUserName)?(
+<AiFillHeart size={30} color={"red"} onClick={NoFavorite}/>
+):(
+<AiOutlineHeart size={30} color={"rgb(38, 38, 38)"} onClick={Favorite}/>
+)}
+</div>
+
+<div style={{margin:"10px 5px 0px 5px"}}>
+<AiOutlineMessage size={30} color={"rgb(38, 38, 38)"} onClick={CommentDisplay}/>
+</div>
+
+<div style={{margin:"5px 5px 5px auto"}}>
+{/* 保存ボタン追加!ログインユーザーのkeepPosts配列(loginUserKeep)に今表示しているpostのpostId(postId)が存在したら保存解除ボタン、存在しなかったら保存するボタン */}
+{loginUserKeep.includes(postid) ? (
+      <RemoveKeepButton postId={postid} size={30} color={"rgb(38, 38, 38)"}/>
+) : (
+      <AddKeepButton postId={postid} size={30} color={"rgb(38, 38, 38)"}/>
+)}
+</div>
+
+</div>
+
+
+<div style ={{fontSize:"18px",marginLeft:"5px"}}>
+いいね！: {favorites.length}人
+</div>
+
+
+<div style ={{fontSize:"16px",margin:"5px"}}>
+<p>{caption}</p>
+</div>
+
+
+<div style={{display:"flex",width:"100%"}}>
+
+<div style={{display:"flex",width:"70%",height:"30px",margin:"5px"}} >
+<input 
+style={{width:"100%"}} 
+type="text" value={inputComment} onChange={(e)=>{setInputComment(e.target.value)}}></input>
+</div>
+
+<div style={{marginLeft:"auto",width:"30%"}}>
+<button onClick={AddComment}>投稿する</button>
+</div>
+
+</div>
+
+{commentDisplay ? (
+      <>
+      <div style={{display:"flex"}}>
+    <div>
+    {displayComment.map((data:any,index:any)=>{
+    return(
+    <div key={index} style={{display:"flex",fontSize:"16px",width:"100%",margin:"3px"}}>
+    <p style={{fontWeight:"500"}}>{data.userName}</p>
+    <p style={{marginLeft:"5px"}}>{data.commentText}</p>
+    </div>
+    )
+    })}
+    </div>
+    <AiOutlineClose  style={{display:"block",margin: "auto 0 0 auto",alignItems:"center"}} size={15} color={"rgb(38, 38, 38)"} onClick={CommentBack} />
+      </div>
+      </>
+):(
+      <>
+      </>
+)}
+
+
+</>
+
+):(
+<>
 <div style={{display:"flex",alignItems:"center",width:"100%"}}>
 
 {/* <Link to="/profile" state={{ userId: userid}}><img src={icon} alt="icon" style ={{width:"100px",height:"100px"}}/></Link> */}
@@ -167,8 +344,9 @@ return (
 <p style ={{fontSize:"20px",marginLeft:"5px"}}>{postUserName }</p>
 
 <div style={{marginLeft:"auto"}}>
-<AiOutlineEllipsis style={{display:"block"}} size={40} color={"rgb(38, 38, 38)"}/>
+<Link to="/"><AiOutlineEllipsis style={{display:"block"}} size={40} color={"rgb(38, 38, 38)"} onClick={Select}/></Link>
 </div>
+
 </div>
 
 
@@ -234,20 +412,9 @@ return (
     )
 })}
 </div>
-
-
-<div>
-{loginUserPost ?(
-<>
-<Link to="/PostEditing" state={{postid:postid,userid:userid}}><button>編集</button></Link>
-<Link to="/"><button onClick={ClickDelition}>削除</button></Link>
-</>
-):(
-<>
 </>
 )}
 </div>
-
 
 <Footer />
 </>
