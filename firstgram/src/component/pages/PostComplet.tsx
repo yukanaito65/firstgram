4dc8aa6ceb067995872d5f56597874d6c5469bfd
emiri yasuo <emiri.yasuo@rakus-partners.co.jp>
2022-12-12 16:21:06 +0900

    sass

--- firstgram/src/component/pages/PostComplet.tsx
+++ firstgram/src/component/pages/PostComplet.tsx
@@ -7,11 +7,17 @@ function PostComplet() {
   return (
     <div>
         <Header show={true} />
-        <div style={{marginTop:"60px"}}>
+        <div className="lead_article"
+        // style={{marginTop:"60px"}}
+        >
         {/* <div style={{width:"100%"}}> */}
-        <p style={{textAlign:"center"}}>投稿が完了しました</p>
+        <p className="lead_center"
+        // style={{textAlign:"center"}}
+        >投稿が完了しました</p>
         {/* </div> */}
-        <div style={{textAlign:"center"}}>
+        <div className="lead_center"
+        // style={{textAlign:"center"}}
+        >
         {/* style={{margin:"0 auto",}} */}
         <Link to="/" ><button className='btn'>Topページに戻る</button></Link>
         </div>
